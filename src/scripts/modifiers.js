36d28f35f5a905c494b85bd65759b15474d8bf0c
stonefarmer9 <g.p.levaillant@gmail.com>
2019-04-17 14:29:36 +0100

    REFACTOR: modifier function

--- src/scripts/modifiers.js
+++ src/scripts/modifiers.js
@@ -1,11 +1,4 @@
 
 exports.modifier = function(number) {
   return Math.floor((number - 10) / 2)
-//   var num = ((number - 10 ) / 2)
-//   if (number > 10) {
-//     console.log(num);
-//    return Math.floor(num)
-//  } else {
-//   return Math.sign(num) * Math.round(Math.sign(num) * num)
-// }
 }
