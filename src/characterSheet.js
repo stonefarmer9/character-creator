0635d397f2eeb5023f2b6a9662ddd0d835548bfb
stonefarmer9 <g.p.levaillant@gmail.com>
2019-04-22 15:30:30 +0100

    FEAT: modifier scores show on character sheet styled too

--- src/characterSheet.js
+++ src/characterSheet.js
@@ -36,13 +36,13 @@ export default class characterSheet extends Component {
 
     const { skills } = this.state;
     const skillList =
-     <ul style={{ listStyleType: "none" }}>
-       <li>{skills.strength}</li>
-       <li>{skills.dexterity}</li>
-       <li>{skills.constitution}</li>
-       <li>{skills.intelligence}</li>
-       <li>{skills.wisdom}</li>
-       <li>{skills.chasrisma}</li>
+     <ul style={{ listStyleType: "none", fontSize:"35px" }}>
+       <li>Strength: {skills.strength} ||--||+{modify.modifier(skills.strength)}</li>
+       <li>Dexterity: {skills.dexterity} ||---||+{modify.modifier(skills.dexterity)}</li>
+       <li>Constitution: {skills.constitution} ||---||+{modify.modifier(skills.constitution)}</li>
+       <li>Intelligence: {skills.intelligence} ||---||+{modify.modifier(skills.intelligence)}</li>
+       <li>Wisdom: {skills.wisdom} ||---||+{modify.modifier(skills.wisdom)}</li>
+       <li>Chasrisma: {skills.charisma} ||---||+{modify.modifier(skills.charisma)}</li>
      </ul>;
 
     const loading = <h2> loading ... </h2>
@@ -50,13 +50,13 @@ export default class characterSheet extends Component {
     return(
       <div className="characterSheet" style={ sectionStyle }>
         <div className="basicsList" style={{ float:"left", backgroundImage: `url(${Background})`, backgroundSize: '100% 100%', width:"500px", height:"500px", marginTop:"20px", marginLeft:"200px"}}>
-          <ul style={{ listStyleType: "none" }}>
-            <li>{this.props.character.name}</li>
-            <li>{this.props.character.age}</li>
-            <li>{this.props.character.race}</li>
-            <li>{this.props.character.classs}</li>
-            <li>{this.props.character.height}</li>
-            <li>{this.props.character.sex}</li>
+          <ul style={{ listStyleType: "none", fontSize:"45px" }}>
+            <li>Name: {this.props.character.name}</li>
+            <li>Age: {this.props.character.age}</li>
+            <li>Race: {this.props.character.race}</li>
+            <li>Class: {this.props.character.classs}</li>
+            <li>Height: {this.props.character.height}</li>
+            <li>Sex: {this.props.character.sex}</li>
           </ul>
         </div>
           <div className="skillsList" style={{ float:"right", backgroundImage: `url(${Background})`, backgroundSize: '100% 100%', width:"500px", height:"500px", marginTop:"20px", marginRight:"200px", marginColor:"#0066ff"}}>
