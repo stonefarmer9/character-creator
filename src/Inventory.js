1a36c18350a16646eb64935042f8c4132a1c87a1
stonefarmer9 <g.p.levaillant@gmail.com>
2019-05-27 13:13:55 +0100

    STYLES: inventory components styled

--- src/Inventory.js
+++ src/Inventory.js
@@ -9,7 +9,13 @@ export default class Inventory extends Component {
       inventory: [{
         name: 'warhammer', value: 100, weight: 15, rarity:'common'},
        { name: 'grappling Hook', value: 10, weight: 3, rarity:'common'},
-       { name: 'holy focus', value: 25, weight: 0.5, rarity: 'uncommon'}]
+       { name: 'holy focus', value: 25, weight: 0.5, rarity: 'uncommon'},
+       { name: 'spellbook', value: 2500, weight: 0.5, rarity: 'rare'},
+       { name: 'gold ring', value: 250, weight: 0.1, rarity: 'uncommon'},
+       { name: 'Heavy Armour', value: 1350, weight: 50, rarity: 'common'},
+       { name: 'lantern', value: 10, weight: 0.7, rarity: 'common'},
+       { name: 'explorers pack', value: 150, weight: 18, rarity: 'common'},
+       { name: 'von Drak blood blade', value: 10000, weight: 15, rarity: 'legendary'}]
     }
   }
 
@@ -33,19 +39,23 @@ export default class Inventory extends Component {
 
     const list = inventory.map(function(item){
       return (
-        <ul className="inventory">
-          <li className="item">Item:{item.name}</li>
-          <li className="item">Value:{item.value}gp</li>
-          <li className="item">Weight:{item.weight}lb</li>
-          <li className="item">Rarity:{item.rarity}</li>
-        </ul>
+        <>
+          <li className="name">{item.name}</li>
+          <li className="value">Value:{item.value}gp</li>
+          <li className="weight">Weight:{item.weight}lb</li>
+          <li className="rarity">Rarity:{item.rarity}</li>
+        </>
       )
     })
-
     return(
       <div>
-      {list}
+      <div className="inventoyScrollList">
+      <ul className="inventory">{list}</ul>
+
+      </div>
+      <div>
       <button className="closeButton" onClick={()=> this.props.showInventory(false)}>Close</button>
+      </div>
       </div>
     )
   }
